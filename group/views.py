5f6c40b4c9c439d827c5a375de6faef9ec7d17b2
Hasanov-Amir <hasanov.amir.81@gmail.com>
2023-05-31 19:21:50 +0400

    group create fix

--- group/views.py
+++ group/views.py
@@ -24,6 +24,15 @@ class GroupView(mixins.CreateModelMixin,
         GroupPermission,
     ]
 
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        data["group_owner_id"] = request.user.id
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
     def list(self, request, *args, **kwargs):
         user_id = request.user.id
         list_of_participating_groups = GroupMember.objects.filter(group_member_id=user_id).values_list("group_id", flat=True)
